a5c29b64864df0c13cff89e6103a6846629b48e6
Ian <ian (at) ianrobinson.net>
2018-03-05 15:24:12 -0500

    Draw smiley elements and add random colors to each element

--- smile.js
+++ smile.js
@@ -2,13 +2,50 @@ window.onload = function() {
   drawSmileyFace();
 }
 
+
+function randomColor() {
+  var colors = ['red', 'blue', 'oragne', 'green', 'pink'];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
 function drawSmileyFace() {
-  var canvas = getElementById("smile");
+  var canvas = document.getElementById("smile");
   var context = canvas.getContext("2d");
   
+  // Big yellow head in the center of the canvas 
   context.beginPath();
-  context.arc(300, 300, 200, 0, degreesToRadians(360), true);
-  context.endPath();
+  context.arc(250, 250, 200, 0, degreesToRadians(360), true);
+  context.fillStyle = randomColor();
+  context.fill();
+  context.stroke();
+  
+  // Left eye
+  context.beginPath();
+  context.arc(175, 225, 40, 0, degreesToRadians(360), true);
+  context.fillStyle = randomColor();
+  context.fill();
+  context.stroke();
+
+  // Right eye
+  context.beginPath();
+  context.arc(326, 225, 40, 0, degreesToRadians(360), true);
+  context.fillStyle = randomColor();
+  context.fill();
+  context.stroke();
+  
+  // Nose
+  context.beginPath();
+  context.arc(250, 300, 20, 0, degreesToRadians(360), true);
+  context.fillStyle = randomColor();
+  context.fill();
+  context.stroke();
+  
+  // Smile
+  context.beginPath();
+  context.arc(250, 200, 200, 20, degreesToRadians(140), false);
+  context.fillStyle = randomColor();
+  context.fill();
+  context.stroke();
 }
 
 function degreesToRadians(deg) {
